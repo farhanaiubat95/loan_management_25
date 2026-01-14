b4c71d136687cc93f2b1d4524b8eff05861ce75e
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2026-01-15 02:13:43 +0600

    Update files

--- config/sslcommerz.php
+++ config/sslcommerz.php
@@ -1,10 +1,24 @@
 <?php
 
-return [
-    'store_id'       => env('SSLCOMMERZ_STORE_ID'),
-    'store_password' => env('SSLCOMMERZ_STORE_PASSWORD'),
-    'sandbox'        => env('SSLCOMMERZ_SANDBOX', true),
+// SSLCommerz configuration
 
-    'sandbox_url' => 'https://sandbox.sslcommerz.com/gwprocess/v4/api.php',
-    'live_url'    => 'https://securepay.sslcommerz.com/gwprocess/v4/api.php',
+$apiDomain = env('SSLCZ_TESTMODE') ? "https://sandbox.sslcommerz.com" : "https://securepay.sslcommerz.com";
+return [
+	'apiCredentials' => [
+		'store_id' => env("SSLCZ_STORE_ID"),
+		'store_password' => env("SSLCZ_STORE_PASSWORD"),
+	],
+	'apiUrl' => [
+		'make_payment' => "/gwprocess/v4/api.php",
+		'transaction_status' => "/validator/api/merchantTransIDvalidationAPI.php",
+		'order_validate' => "/validator/api/validationserverAPI.php",
+		'refund_payment' => "/validator/api/merchantTransIDvalidationAPI.php",
+		'refund_status' => "/validator/api/merchantTransIDvalidationAPI.php",
+	],
+	'apiDomain' => $apiDomain,
+	'connect_from_localhost' => env("IS_LOCALHOST", false), // For Sandbox, use "true", For Live, use "false"
+	'success_url' => '/success',
+	'failed_url' => '/fail',
+	'cancel_url' => '/cancel',
+	'ipn_url' => '/ipn',
 ];
